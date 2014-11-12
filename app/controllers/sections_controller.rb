379e86edd2987268eb426debe900d6d5dd9122c0
James Steinigner <jesteininger@gmail.com>
2014-11-12 20:05:57 +0000

    Wishlist production different than local

--- app/controllers/sections_controller.rb
+++ app/controllers/sections_controller.rb
@@ -26,7 +26,8 @@ class SectionsController < ApplicationController
   def wishlist
     if params[:search]
       #@sections = Section.where(['teacher LIKE ? or room LIKE ?', "%#{params[:search]}%", "%#{params[:search]}%"])
-      @sections = Section.where(['section_name_and_title LIKE ? or teacher LIKE ? or room LIKE ?', "%#{params[:search]}%", "%#{params[:search]}%", "%#{params[:search]}%"]).paginate(:page => params[:page], :per_page => 30)
+    #  @sections = Section.where(['section_name_and_title LIKE ? or teacher LIKE ? or room LIKE ?', "%#{params[:search]}%", "%#{params[:search]}%", "%#{params[:search]}%"]).paginate(:page => params[:page], :per_page => 30)
+@sections = Section.where(['section_name_and_title LIKE ?', "%#{params[:search]}%"]).paginate(:page => params[:page], :per_page => 30)
      # @section_majors = Major.where(['name LIKE ?', "%#{params[:search]}%"])
     #  @major_sections = Section.joins(:majors).where(['majors.name LIKE ?', "%#{params[:search]}%"])
     #  @sections = @sections + @major_sections
